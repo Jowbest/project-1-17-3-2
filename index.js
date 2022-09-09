c1720a52a235ea8f0db0351bd9d49eabe33e2095
Suphadat Chonlatep <bassrtv1@gmail.com>
2022-09-09 17:05:13 +0700

    ใส่VantaJs และ Footer

---
+++ index.js
@@ -0,0 +1,11 @@
+VANTA.GLOBE({
+  el: "#page-header",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  backgroundColor: 0xcd81b1
+})
